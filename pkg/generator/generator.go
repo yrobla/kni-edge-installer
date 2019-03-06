00e79bfbf0da4b62bb455d188fc39373a31c91ec
Yolanda Robla <yroblamo@redhat.com>
2019-03-06 19:48:31 +0000

    Add clone of private git repo with key

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,7 +1,10 @@
 package generator
 
 import (
+    "encoding/base64"
     "fmt"
+    "io/ioutil"
+    "log"
     "os"
 
     getter "github.com/hashicorp/go-getter"
@@ -22,15 +25,34 @@ func New(baseRepo string, basePath string, installerPath string, secretsRepo str
 }
 
 func (g generator) GenerateFromInstall() {
+    // Download installer for openshift
+    log.Println("Downloading openshift-install binary")
     binaryPath := fmt.Sprintf("%s/openshift-install", g.buildPath)
-    client := &getter.Client{
-        Src: g.installerPath,
-        Dst: binaryPath,
-    }
+    client := &getter.Client{Src: g.installerPath, Dst: binaryPath}
     err := client.Get()
     if err != nil {
-        fmt.Println(err)
+        log.Fatal(fmt.Sprintf("Error downloading openshift-install binary: %s", err))
         os.Exit(1)
     }
     os.Chmod(binaryPath, 0777)
+
+    // Download the credentials repo
+    log.Println("Download secrets repo")
+    secretsPath := fmt.Sprintf("%s/secrets", g.buildPath)
+
+    // Retrieve private key and b64encode it
+    rsaPrivateLocation := fmt.Sprintf("%s/.ssh/id_rsa", os.Getenv("HOME"))
+    priv, _ := ioutil.ReadFile(rsaPrivateLocation)
+    sEnc := base64.StdEncoding.EncodeToString(priv)
+
+    finalUrl := fmt.Sprintf("%s?sshkey=%s", g.secretsRepo, sEnc)
+    log.Println(finalUrl)
+    client = &getter.Client{Src: finalUrl, Dst: secretsPath, Mode: getter.ClientModeAny}
+    err = client.Get()
+    if err != nil {
+        log.Println(secretsPath)
+        log.Fatal(fmt.Sprintf("Error downloading secrets repo: %s", err))
+        os.Exit(1)
+    }
+    os.Chmod(secretsPath, 0700)
 }
